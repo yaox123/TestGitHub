75deee26d0fe4aa1e75d66889675811edef5c7a2
yaox <974526541@qq.com>
2018-10-23 15:00:43 +0800

    第三次提交

---
+++ src/com/Test3.java
@@ -0,0 +1,13 @@
+package com;
+
+/**
+ * @BelongsProject: TestGitHub
+ * @BelongsPackage: com
+ * @Author: yaox
+ * @CreateTime: 2018-10-23 15:00
+ */
+public class Test3 {
+    public static void main(String[] args) {
+        System.out.println(3333333);
+    }
+}
