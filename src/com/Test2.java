ac5e08b2079e30c0a07a4e702695c50622f58dd9
yaox <974526541@qq.com>
2018-10-23 14:56:00 +0800

    第二次提交

---
+++ src/com/Test2.java
@@ -0,0 +1,13 @@
+package com;
+
+/**
+ * @BelongsProject: TestGitHub
+ * @BelongsPackage: com
+ * @Author: yaox
+ * @CreateTime: 2018-10-23 14:54
+ */
+public class Test2 {
+    public static void main(String[] args) {
+        System.out.println(2222222);
+    }
+}
