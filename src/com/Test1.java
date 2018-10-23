cf79da837cdaf17e1a2a1836dc90bd975dd899be
yaox <974526541@qq.com>
2018-10-23 14:53:04 +0800

    第一次提交

---
+++ src/com/Test1.java
@@ -0,0 +1,13 @@
+package com;
+
+/**
+ * @BelongsProject: TestGitHub
+ * @BelongsPackage: com
+ * @Author: yaox
+ * @CreateTime: 2018-10-23 14:48
+ */
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.println(111111);
+    }
+}
